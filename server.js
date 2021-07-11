c3467fa3ac9fe36d63d9c15f5ef5ba77dd07309a
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-07-11 16:24:34 -0400

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
 
-// Bodyparser Middleware
-app.use(bodyParser.json());
+
+app.use(express.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 // User Routes
-
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
